89cd87209f69d373802e494d3d9921a5b92259d8
Azizul Hakim Akib <akib1162100@gmail.com>
2022-11-29 20:07:22 +0600

    verify debug

--- utils/token_verify.py
+++ utils/token_verify.py
@@ -6,6 +6,8 @@ def verify_token(req: Request):
     token = req.headers.get("Secret_Code")
     if token:
         token = token.replace("\"","")
+    print(token)
+    print(f"Secret {SECRET}" )
     valid = (token == SECRET)
     if not valid:
         raise HTTPException(status_code=401, detail="Unauthorized")
